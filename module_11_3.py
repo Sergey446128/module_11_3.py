6f151cc5244675c356fad54d47560ecda83a463e
Sergey446128 <s.milyaev2018@yandex.ru>
2024-12-16 16:08:31 +0300

    Add files via upload

---
+++ module_11_3.py
@@ -0,0 +1,22 @@
+def introspection_info(obj):
+    # Определение типа объекта
+    obj_type = type(obj).__name__
+
+    # Получение атрибутов объекта
+    attributes = dir(obj)
+
+    # Получение методов объекта
+    methods = [method for method in attributes if callable(getattr(obj,method))]
+
+    # Определение модуля, к которому объект пренадлежит
+    module = obj.__class__.__module__
+
+    # Создание словаря с информацией об объекте
+    info = {'type': obj_type, 'attributes': attributes, 'methods': methods, 'module': module},
+
+    return info
+
+# Интроспекция числа
+number_info = introspection_info(42)
+print(number_info)
+
